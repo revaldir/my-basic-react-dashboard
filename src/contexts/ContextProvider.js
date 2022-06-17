6a7df2c1ec93aa73c6ab455f49a5fdaa63398739
Revaldi <revaldii32@gmail.com>
2022-06-17 18:45:07 +0700

    [IMP] - Finish Navbar plus Adding New Contexts

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -6,17 +6,28 @@ const initialState = {
   chat: false,
   cart: false,
   userProfile: false,
-  notifications: false
+  notification: false
 }
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true});
+  }
 
   return (
     <StateContext.Provider 
       value={{ 
         activeMenu,
-        setActiveMenu
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize
        }}
     >
       {children}
