d09b18abd8e8bfe5ce41fbe6da3a80727e4ab34f
Revaldi <revaldii32@gmail.com>
2022-06-20 15:50:49 +0700

    [IMP] - Add Datatables using Syncfusion + Implementations on Orders, Employees, Customers Page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Inject, Page, Search, Toolbar } from '@syncfusion/ej2-react-grids';
+
+import { Header } from '../components';
+import { employeesGrid, employeesData } from '../data/dummy';
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
   )
 }
 
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Inject, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit } from '@syncfusion/ej2-react-grids';
+
+import { Header } from '../components';
+import { ordersGrid, ordersData, contextMenuItems } from '../data/dummy';
 
 const Orders = () => {
   return (
-    <div>Orders</div>
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, Filter, Page, ExcelExport, PdfExport, Edit, ContextMenu]} />
+      </GridComponent>
+    </div>
   )
 }
 
